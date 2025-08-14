7008ea65c8ea2685fbe0f5d97789b5330817a823
Isaac025 <isaacolayiwola85@gmail.com>
2025-08-14 07:30:26 -0700

    updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -100,23 +100,7 @@ const loginUser = async (req, res) => {
   }
 };
 
-const getUser = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const user = await USER.findById(id);
 
-    if (!user) {
-      return res
-        .status(404)
-        .json({ success: false, message: "User not found" });
-    }
-
-    res.status(200).json({ success: true, user });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: error.message });
-  }
-};
 
 module.exports = {
   registerUser,
